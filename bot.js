d231d716588fdc779f953cad3f61a184b783014f
asfdsjfskdvkx <42547466+asfdsjfskdvkx@users.noreply.github.com>
2018-08-20 19:29:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,6 +77,28 @@ if (message.content.startsWith(prefix + 'setimg')) {
 }
 });
 
+lient.on("message", message => {
+    var prefix = "$";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+ if (!args[1]) {
+                                let embed3 = new Discord.RichEmbed()
+                                .setDescription(":white_check_mark: | تم ارسال للكل رساله فارغه")
+                                .setColor("#FF0000")
+                                message.channel.sendEmbed(embed3);
+                            } else {
+
+                            let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال للكل الرساله !')
+                                                            .setColor("#008000")
+                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+
 
  client.on('message', msg =>{
     let message=msg;
